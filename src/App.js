7b72f6455ea5edd2bf03cc1fdf223abd790c09ab
NinjaDev <68790388+ninjaaGit@users.noreply.github.com>
2021-11-19 17:35:43 -0300

    Number Mask

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {IndexContext} from '../src/context/index'
 import QRCodeCanvas from './QRCodeCanvas';
 import Input from '@mui/material/Input';
 import Button from '@mui/material/Button';
+import jSuites from "jsuites";
 import {ReactComponent as DownloadSVG} from './SVGs/download.svg'
 import {ReactComponent as LogoSVG} from './SVGs/logo.svg'
 
@@ -15,9 +16,9 @@ function App() {
     <div className="QRCodeDivAll">
         <div className="QRCodeDivInput">
             <LogoSVG/>
-            <Input color="success" className="QRCodeInput" value={localStorage.getItem('nome')} id="QRNome" placeholder="Nome" onChange={(element) => setNome(element.target.value)}></Input>
-            <Input color="success" className="QRCodeInput" value={localStorage.getItem('email')} type="email" id="QREmail" placeholder="Email" onChange={(element) => setEmail(element.target.value)}></Input>
-            <Input color="success" className="QRCodeInput" value={localStorage.getItem('numero')} type="number" id="QRNumero" placeholder="Celular" onChange={(element) => setNumero(element.target.value)}></Input>
+            <Input color="success" disableUnderline="true" className="QRCodeInput" value={localStorage.getItem('nome')} id="QRNome" placeholder="Nome" onChange={(element) => setNome(element.target.value)}></Input>
+            <Input color="success" disableUnderline="true" className="QRCodeInput" value={localStorage.getItem('email')} type="email" id="QREmail" placeholder="Email" onChange={(element) => setEmail(element.target.value)}></Input>
+            <input color="success" data-mask='(00) 00000-0000' maxlength="15" className="QRCodeInput" value={localStorage.getItem('numero')} type="tel" id="QRNumero" placeholder="Celular" onChange={(element) => setNumero(element.target.value)}></input>
         </div>
         <div className="QRCodeDivContainerPai">
           <div className="QRCodeDivContainer">
