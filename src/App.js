22b27acd5a62d5d17004c22ffb44a463d69e6bc2
NinjaDev <68790388+ninjaaGit@users.noreply.github.com>
2021-11-23 17:04:48 -0300

    Testando Share

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import {IndexContext} from '../src/context/index'
 import QRCodeCanvas from './QRCodeCanvas';
 import Input from '@mui/material/Input';
 import Button from '@mui/material/Button';
+import {WhatsappShareButton} from "react-share";
+import {WhatsappIcon} from "react-share";
 import jSuites from "jsuites";
 import {ReactComponent as DownloadSVG} from './SVGs/download.svg'
+import {ReactComponent as ShareSVG} from './SVGs/share.svg'
 import {ReactComponent as LogoSVG} from './SVGs/logo.svg'
 
 function App() {
-
-  const { setNome, setEmail, setNumero, handleSave, vcard} = React.useContext(IndexContext);
-
+  const { setNome, setEmail, setNumero, setCargo, setFixo, handleSave, handleShare, vcard, isMobile, impcomp} = React.useContext(IndexContext);  
+  
   return (
     <div className="QRCodeDivAll">
         <div className="QRCodeDivInput">
             <LogoSVG/>
             <Input color="success" disableUnderline="true" className="QRCodeInput" value={localStorage.getItem('nome')} id="QRNome" placeholder="Nome" onChange={(element) => setNome(element.target.value)}></Input>
             <Input color="success" disableUnderline="true" className="QRCodeInput" value={localStorage.getItem('email')} type="email" id="QREmail" placeholder="Email" onChange={(element) => setEmail(element.target.value)}></Input>
-            <input color="success" data-mask='(00) 00000-0000' maxlength="15" className="QRCodeInput" value={localStorage.getItem('numero')} type="tel" id="QRNumero" placeholder="Celular" onChange={(element) => setNumero(element.target.value)}></input>
+            <Input color="success" disableUnderline="true" className="QRCodeInput" value={localStorage.getItem('cargo')} id="QRCargo" placeholder="Cargo" onChange={(element) => setCargo(element.target.value)}></Input>
+            <input color="success" data-mask='(00) 00000-0000' maxLength="15" className="QRCodeInput" value={localStorage.getItem('numero')} type="tel" id="QRNumero" placeholder="Celular" onChange={(element) => setNumero(element.target.value)}></input>
+            <input color="success" data-mask='(00) 0000-0000' maxLength="14" className="QRCodeInput" value={localStorage.getItem('fixo')} type="tel" id="QRFixo" placeholder="Fixo (Opcional)" onChange={(element) => setFixo(element.target.value)}></input>
         </div>
         <div className="QRCodeDivContainerPai">
           <div className="QRCodeDivContainer">
@@ -27,10 +31,20 @@ function App() {
                 <QRCodeCanvas text={vcard}></QRCodeCanvas>
                 <h1 className="QRCodeNome">{localStorage.getItem('nome') || "Seu Nome"}</h1>
             </div>
-            <Button color="success" onClick={handleSave}>
-              DOWNLOAD
-              <DownloadSVG/>
-            </Button>
+            {isMobile ?
+              <WhatsappShareButton
+                url={localStorage.getItem('img')}
+                title={localStorage.getItem('fixo')}
+                separator=":: "> 
+                <Button color="success">
+                  COMPARTILHAR<WhatsappIcon size={20} round/>
+                </Button>
+              </WhatsappShareButton>:
+              <Button color="success" onClick={handleSave}>
+                DOWNLOAD
+                <DownloadSVG/>
+              </Button>
+            }
           </div>
         </div>
     </div>
--- src/context/index.js
+++ src/context/index.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState} from "react";
+import React, { createContext, useState, useEffect} from "react";
 import domtoimage from 'dom-to-image';
 import { jsPDF } from "jspdf";
 export const IndexContext = createContext();
@@ -9,10 +9,15 @@ export default function IndexProvider({ children }) {
     const [nome, setNome] = useState(localStorage.getItem('nome'))
     const [email, setEmail] = useState(localStorage.getItem('email'))
     const [numero, setNumero] = useState(localStorage.getItem('numero'))
+    const [fixo, setFixo] = useState(localStorage.getItem('fixo'))
+    const [cargo, setCargo] = useState(localStorage.getItem('cargo'))
+    const [isMobile, setIsMobile] = React.useState(false)
 
     localStorage.setItem('nome', nome)
     localStorage.setItem('email', email)
     localStorage.setItem('numero', numero)
+    localStorage.setItem('fixo', fixo)
+    localStorage.setItem('cargo', cargo)
 
     if (nome === null) {
       setNome("")
@@ -23,6 +28,20 @@ export default function IndexProvider({ children }) {
     if (numero === null) {
       setNumero("")
     }
+    if (fixo === null) {
+      setFixo("")
+    }
+    if (cargo === null) {
+      setCargo("")
+    }
+
+    useEffect(() => {      
+      if (window.matchMedia("(min-width: 768px)").matches) {
+        setIsMobile(false)
+      } else {
+        setIsMobile(true)
+      }
+    })
 
     var vcard_begin = 'BEGIN:VCARD\nVERSION:3.0\n';
     
@@ -36,18 +55,23 @@ export default function IndexProvider({ children }) {
     
     var telefoneV = 'TEL;TYPE=CELL:' + numero + '\n';
 
+    var fixoV = 'TEL;TYPE=HOME:' + fixo + '\n';
+
     var orgV = 'ORG:' + "Arcom" + '\n';
+
+    var roleV = 'TITLE:' + cargo + '\n';
 
     var urlV = 'URL;TYPE=WORK:' + "https://www.arcom.com.br" + '\n';
 
-    var adrV = "ADR;TYPE=WORK:" + "Arcom - Anel Viario Ayrton Senna, 2001 - Distrito Industrial, Uberlandia - MG, 38402-329" + '\n'
+    var adrV = "ADR;TYPE=WORK:" + 'Arcom - Anel Viario Ayrton Senna, 2001 - Distrito Industrial, Uberlândia - MG 38402-329' + '\n'
 
-    var vcard = vcard_begin+nomeV+nV+emailV+telefoneV+orgV+adrV+urlV+vcard_end;
+    var vcard = vcard_begin+nomeV+nV+emailV+telefoneV+fixoV+orgV+roleV+adrV+urlV+vcard_end;
+
+    console.log(vcard)
 
     function handleSave() {
       var node = document.getElementById('dados');
-      var imgTeste = node[0].toDataURL("image/png")
-      domtoimage.toPng(imgTeste).then(function (dataUrl) {
+      domtoimage.toPng(node).then(function (dataUrl) {
         var img = new Image();
         var doc = new jsPDF({
           orientation: "portrait",
@@ -57,7 +81,8 @@ export default function IndexProvider({ children }) {
         const link = document.createElement("a");
         var imgcomp = img.src = dataUrl;
         link.href = imgcomp
-        console.log(typeof(imgcomp));
+        localStorage.setItem('img', link)
+        console.log(imgcomp)
         doc.addImage(imgcomp, 'PNG', 1, 1)
         doc.save('vCard ' + localStorage.getItem('nome') + '.pdf')
         // link.setAttribute("download", "VCard " + localStorage.getItem('nome') + ".png")
@@ -68,7 +93,12 @@ export default function IndexProvider({ children }) {
       });
     }
 
-return (<IndexContext.Provider value={{text, setText, nome, setNome, email, setEmail, numero, setNumero, handleSave, vcard}}>
-    {children}
-  </IndexContext.Provider>);
+    function handleShare(){
+      
+    }
+
+return (<IndexContext.Provider value={{text, setText, nome, setNome, email, setEmail, numero, setNumero, cargo, setCargo, fixo, setFixo, handleSave, handleShare, vcard, isMobile}}>
+          {children}
+        </IndexContext.Provider>
+       );
 }
